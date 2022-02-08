e5e19b1bdb4dbbf7d0a58685d3a3565b570d864f
Sarah Beckett <shbeckett@gmail.com>
2022-02-08 12:58:43 +0000

    finally worked out where changes were saved

--- Exercise10/ex10_starter.py
+++ Exercise10/ex10_starter.py
@@ -8,13 +8,16 @@ else:
 
 # Construct a portable wildcard pattern
 pattern = os.path.join(hdir,'*')
-
 # Use the glob.glob() function to obtain the list of filenames
 mystuff = glob.glob(pattern)
 for things in mystuff:
-    # a test to only display files that are not zero length - used fact 0 evaluates as false
+ #shows file sizes - initially went wrong here and printed the whole of mystuff for each iteration...
+ print(things)
+ print(os.path.getsize(things))
+ # a test to only display files that are not zero length - used fact 0 evaluates as false
+for things in mystuff:
  if os.path.getsize(things):
-     # Remove the leading directory name(s) from each filename before you print it
+  # Remove the leading directory name(s) from each filename before you print it
   print(os.path.basename(things))
 
 
